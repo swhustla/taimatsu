aad8e6364c805b0cb19731eae2a711d41ecda393
swhustla <fdkelly@gmail.com>
2023-01-31 11:47:23 +0000

    Add tsetlin machine to data loader

--- taimatsu/data.py
+++ taimatsu/data.py
@@ -8,10 +8,15 @@ from torchvision import transforms
 from torch import cuda
 from torch.backends import cudnn
 
+from pyTsetlinMachine.tools import Binarizer
+from pyTsetlinMachine.tm import MultiClassTsetlinMachine
+
 from taimatsu.utils import to_categorical
 from taimatsu.utils.options import get_device
 from taimatsu.resnet import ResNet18
 from taimatsu.model import Model
+
+import pandas as pd
 
 from sklearn import datasets as skl_datasets
 from torchvision import datasets as tv_datasets
@@ -21,20 +26,29 @@ device = get_device()
 
 
 class DataLoaderFetcher:
-    def __init__(self, name: str = "Iris"):
+    def __init__(self, name: str = "Iris", model_name: str = "ResNet18"):
         self.name = name
+        self.model_name = model_name
 
     # TODO: Handle error in case user passes an unsupported dataset name
     def train_loader(self) -> DataLoader:
 
         # TODO: make Iris the default
         if self.name == "Iris":
-            return DataLoader(
-                self.dataset(train=True),
-                batch_size=40,
-                shuffle=True,
-                drop_last=False,
-            )
+            if self.model_name == "ResNet18":
+                return DataLoader(
+                    self.dataset(train=True),
+                    batch_size=40,
+                    shuffle=True,
+                    drop_last=False,
+                )
+            elif self.model_name == "TsetlinMachine":
+                return DataBinarizer(DataLoader(
+                    self.dataset(train=True),
+                    batch_size=40,
+                    shuffle=True,
+                    drop_last=False,
+                ))
 
         elif self.name == "Wine":
             return DataLoader(
@@ -107,26 +121,47 @@ class DataLoaderFetcher:
             )
 
     def model(self):
-        if self.name == "Iris":
-            model = Model(
-                n_features=self.dataset().number_of_predictors(),
-                n_neurons=10,
-                n_out=self.dataset().number_of_categories(),
-            )
-        elif self.name == "Wine":
-            model = Model(
-                n_features=self.dataset().number_of_predictors(),
-                n_neurons=10,
-                n_out=self.dataset().number_of_categories(),
-            )
-        elif self.name == "MNIST":
-            model = ResNet18(in_channels=1, num_classes=10)
-        model = model.to(device)
-        if cuda.is_available():
-            cudnn.benchmark = True
-            # TODO: Use torch.nn.parallel.DistributedDataParallel
-            model = DataParallel(model)
-        return model
+        if self.model_name == "ResNet18":
+            if self.name == "Iris":
+                model = Model(
+                    n_features=self.dataset().number_of_predictors(),
+                    n_neurons=10,
+                    n_out=self.dataset().number_of_categories(),
+                )
+            elif self.name == "Wine":
+                model = Model(
+                    n_features=self.dataset().number_of_predictors(),
+                    n_neurons=10,
+                    n_out=self.dataset().number_of_categories(),
+                )
+            elif self.name == "MNIST":
+                model = ResNet18(in_channels=1, num_classes=10)
+            model = model.to(device)
+            if cuda.is_available():
+                cudnn.benchmark = True
+                # TODO: Use torch.nn.parallel.DistributedDataParallel
+                model = DataParallel(model)
+            return model
+        elif self.model_name == "TsetlinMachine":
+            if self.name == "Iris":
+                threshold_large_t = 10  # Tsetlin Machine threshold
+                boost_factor_s = 3.0 # Tsetlin Machine boost factor
+                number_of_clauses = 300 # Number of clauses per TM
+                number_of_states = 100 # Number of states per TM
+
+                model = MultiClassTsetlinMachine(
+                    self.dataset().number_of_categories(),
+                    number_of_clauses,
+                    self.dataset().number_of_predictors(),
+                    number_of_states,    
+                    boost_factor_s,
+                    threshold_large_t,
+                    boost_true_positive_feedback = 1
+                )
+                return model
+        else:
+            raise RuntimeError(f"Model {self.model_name} not supported with dataset {self.name}")
+
 
 
 class TabularDataSet(Dataset):
@@ -177,3 +212,39 @@ class TabularDataSet(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+
+
+
+class DataBinarizer(DataLoader):
+    """A class to binarize the data, aimed at the Tsetlin Machine."""
+    
+    def __init__(self, dataset, batch_size=1, shuffle=False, num_workers=0):
+        super().__init__(dataset, batch_size, shuffle, num_workers)
+
+    def __iter__(self):
+        for predictors, categories in super().__iter__():
+            binarized_predictors = self.__binarize_data(predictors)
+            yield binarized_predictors, categories
+
+    @staticmethod
+    def __binarize_data(data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Binarize the data.
+
+        The Tsetlin Machine requires the data to be binarized,
+        i.e. to be represented as a set of 0s and 1s, due to the way that it works.
+        """
+        binarizer = Binarizer(max_bits_per_feature=10)
+        np_array_version_of_data = np.array(data)
+        binarizer.fit(np_array_version_of_data)
+        binarized_data_array = binarizer.transform(np_array_version_of_data)
+        new_binarized_columns = [
+            f"binarized_{i}" for i in range(binarized_data_array.shape[1])
+        ]
+        return pd.DataFrame(
+            binarized_data_array, columns=new_binarized_columns
+        )
+    
+    
